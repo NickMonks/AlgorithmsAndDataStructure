4ea5e4c5f00e759108f13542167b38a0a71a5ff4
NickMonks <nicolasmg94@gmail.com>
2021-05-29 15:05:14 +0100

    Common Ancestor and Invert BT solved

---
+++ InvertBTree.py
@@ -0,0 +1,71 @@
+import unittest
+    
+    # Time: O(n) | Space: O(d)
+def invertBinaryTree(tree):
+    # TIP: when traversing the binary tree, is better to put the condition
+    # where the tree that we're checking (tree.left doesnt exists, is None)
+    # Do this check; DONT do if tree.left is None and ...
+    if tree is None:
+        return
+
+    swapLeftAndRight(tree)
+    invertBinaryTree(tree.left)
+    invertBinaryTree(tree.right)
+
+def swapLeftAndRight(tree):
+    tree.left, tree.right = tree.right, tree.left
+
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __eq__(self, other):
+        return isinstance(other, type(self)) and self.__dict__ == other.__dict__
+
+    def insert(self, values, i=0):
+        if i >= len(values):
+            return
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            if current.left is None:
+                current.left = BinaryTree(values[i])
+                break
+            queue.append(current.left)
+            if current.right is None:
+                current.right = BinaryTree(values[i])
+                break
+            queue.append(current.right)
+        self.insert(values, i + 1)
+        return self
+
+    def invertedInsert(self, values, i=0):
+        if i >= len(values):
+            return
+        queue = [self]
+        while len(queue) > 0:
+            current = queue.pop(0)
+            if current.right is None:
+                current.right = BinaryTree(values[i])
+                break
+            queue.append(current.right)
+            if current.left is None:
+                current.left = BinaryTree(values[i])
+                break
+            queue.append(current.left)
+        self.invertedInsert(values, i + 1)
+        return self
+
+
+class TestProgram(unittest.TestCase):
+    def test_case_1(self):
+        tree = BinaryTree(1).insert([2, 3, 4, 5, 6, 7, 8, 9])
+        invertedTree = BinaryTree(1).invertedInsert([2, 3, 4, 5, 6, 7, 8, 9])
+        invertBinaryTree(tree)
+        self.assertTrue(tree.__eq__(invertedTree))
+
+
+if __name__ == "__main__":
+    unittest.main()
